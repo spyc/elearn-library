e9c5ccdf0cf60297d52b9894536cbf0214cbae0b
Tony Yip <tony@opensource.hk>
2015-10-23 22:29:55 +0800

    Update Version

---
+++ src/resources/lang/en/common.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'name' => 'SPYC Library'
+];
--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,34 +1,38 @@
 @extends('library::template')
 
 @section('wrap')
-    <div class="container" style="padding-top: 0;">
-        <div class="jumbotron text-center" id="introjumbo">
-            <h1>SPYC Library</h1>
-            <hr />
-            <p>2/F, New Wing,<br />Shatin Pui Ying College</p>
+    <div class="jumbotron" id="introjumbo">
+        <div class="container">
+            <h1>{{ trans('library::common.name') }}</h1>
         </div>
     </div>
-    <div class="dark-sections">
-        <div class="container">
-            <div class="row">
-                <div class="col-sm-7">
-                    <h2>OPAC</h2>
-                    <iframe src="http://library.pyc.edu.hk/WebOPAC.exe" width="600px" height="400px">OPAC</iframe>
-                </div>
-                <div class="col-sm-3">
-                    <a href="http://www.pyc.edu.hk/~chin/reader/index.html" target="_blank">
-                        <img src='{{ url('/images/library/chin.jpg') }}' class="img-rounded link" alt="小讀者園地">
-                    </a>
-                    <br>
-                    <a href="https://school.pyc.edu.hk" target="_blank">
-                        <img src="{{ url('/images/pycnet.jpg') }}" class="img-rounded link" alt="PYCnet">
-                    </a>
-                    <br>
-                    <a href="https://ststpyc.wisenews.net/" target="_blank">
-                        <img src="{{ url('/images/library/wisenews.jpg') }}" class="img-rounded link" alt="WiseNews">
-                    </a>
-                </div>
+    <div class="container">
+        <div class="embed-responsive embed-responsive-4by3" style="padding-bottom: 350px;">
+            <iframe class="embed-responsive-item" src="http://library.pyc.edu.hk/WebOPAC.exe" style="max-height: 500px;">OPAC</iframe>
+        </div>
+
+
+        <div class="row">
+            <div class="col-sm-4">
+                <a href="http://www.pyc.edu.hk/~chin/reader/index.html" target="_blank">
+                    <img src='{{ url('/images/library/chin.jpg') }}' class="img-rounded link" alt="小讀者園地">
+                </a>
+            </div>
+
+            <div class="col-sm-4">
+                <a href="https://school.pyc.edu.hk" target="_blank">
+                    <img src="{{ url('/images/pycnet.jpg') }}" class="img-rounded link" alt="PYCnet">
+                </a>
+            </div>
+
+            <div class="col-sm-3">
+                <a href="https://ststpyc.wisenews.net/" target="_blank">
+                    <img src="{{ url('/images/library/wisenews.jpg') }}" class="img-rounded link" alt="WiseNews">
+                </a>
             </div>
         </div>
+    </div>
+
+    <div class="container">
     </div>
 @stop
